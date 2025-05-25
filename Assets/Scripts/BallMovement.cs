2fc3057610cf692d3cfffd780b7d9528439cdb3e
Regal Adhikari <65963700+RegalAdhikari@users.noreply.github.com>
2025-05-25 10:08:39 +0545

    touch update

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -14,33 +14,38 @@ public class BallMovement : MonoBehaviour
 
     [SerializeField] private GameObject jumpDetector;
     [SerializeField] private GroundCheck groundCheck;
-    
+
 
     private float coyoteTime = 0.1f;
     private float coyoteTimeCounter = 0f;
     private float jumpBufferCounter;
-    private float jumpBufferTime = 0.2f; 
+    private float jumpBufferTime = 0.2f;
     private Rigidbody2D rb2d;
     private float movX;
-
+    private Vector3 fp;   //First touch position
+    private Vector3 lp;   //Last touch position
+    private float dragDistance;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        dragDistance = Screen.height * 15 / 100; //dragDistance is 15% height of the screen
+
     }
 
 
     void Update()
     {
-        
+        TouchUpdate();
         jumpDetector.transform.rotation = Quaternion.identity;
-        jumpDetector.transform.position = new Vector3(transform.position.x, transform.position.y -0.55f, 0f);
+        jumpDetector.transform.position = new Vector3(transform.position.x, transform.position.y - 0.55f, 0f);
         // movX = Input.GetAxis("Horizontal"); // Comment out for release
         movX = moveValue;
-        if(groundCheck.isGrounded)
+        if (groundCheck.isGrounded)
         {
             coyoteTimeCounter = coyoteTime;
         }
-        else{
+        else
+        {
             coyoteTimeCounter -= Time.deltaTime;
         }
         if (Input.GetKeyDown(KeyCode.Space))
@@ -49,14 +54,14 @@ public class BallMovement : MonoBehaviour
         }
         else
         {
-            jumpBufferCounter-=Time.deltaTime;
+            jumpBufferCounter -= Time.deltaTime;
         }
-        if (coyoteTimeCounter>0f && jumpBufferCounter>0f)
+        if (coyoteTimeCounter > 0f && jumpBufferCounter > 0f)
         {
-        //   Jump();
-        rb2d.linearVelocityY = jumpSpeed;
-        coyoteTimeCounter = 0f;
-        jumpBufferCounter = 0f;
+            //   Jump();
+            rb2d.linearVelocityY = jumpSpeed;
+            coyoteTimeCounter = 0f;
+            jumpBufferCounter = 0f;
         }
     }
 
@@ -64,7 +69,7 @@ public class BallMovement : MonoBehaviour
     {
         Move();
         // If the ball goes too down
-        if (transform.position.y<=-10f)
+        if (transform.position.y <= -10f)
         {
             rb2d.linearVelocity = Vector3.zero;
             transform.position = spawnTransform.position;
@@ -75,7 +80,7 @@ public class BallMovement : MonoBehaviour
     {
         if (rb2d.linearVelocityX < Math.Abs(maxMoveSpeed))
         {
-            rb2d.linearVelocityX += moveSpeed * movX* Time.fixedDeltaTime;
+            rb2d.linearVelocityX += moveSpeed * movX * Time.fixedDeltaTime;
         }
         else
         {
@@ -83,17 +88,86 @@ public class BallMovement : MonoBehaviour
         }
     }
 
-    private void Jump()
+    public void Jump()
     {
-        if (groundCheck.isGrounded)
-        {
-            rb2d.linearVelocityY = jumpSpeed;
-        }
+        // if (groundCheck.isGrounded)
+        // {
+        //     rb2d.linearVelocityY = jumpSpeed;
+        // }
+        jumpBufferCounter = jumpBufferTime;
+
     }
-    public void movePress(float value){
+    public void movePress(float value)
+    {
         moveValue = value;
     }
-    public void moveRelease(){
+    public void moveRelease()
+    {
         moveValue = 0;
+    }
+    private void TouchUpdate()
+    {
+        if (Input.touchCount == 1) // user is touching the screen with a single touch
+        {
+            Touch touch = Input.GetTouch(0); // get the touch
+            if (touch.phase == TouchPhase.Began) //check for the first touch
+            {
+                fp = touch.position;
+                lp = touch.position;
+                if (fp.x < Screen.width / 2)
+                {
+                    Debug.Log("Move left");
+                    moveValue = -1;
+                }
+                else
+                {
+                    Debug.Log("Move right");
+                    moveValue = 1;
+                }
+            }
+
+            else if (touch.phase == TouchPhase.Moved) // update the last position based on where they moved
+            {
+                lp = touch.position;
+                moveValue = 0;
+
+            }
+            else if (touch.phase == TouchPhase.Ended) //check if the finger is removed from the screen
+            {
+                moveValue = 0;
+
+                lp = touch.position;  //last touch position. Ommitted if you use list
+
+                //Check if drag distance is greater than 20% of the screen height
+                if (Mathf.Abs(lp.x - fp.x) > dragDistance || Mathf.Abs(lp.y - fp.y) > dragDistance)
+                {//It's a drag
+                 //check if the drag is vertical or horizontal
+                    if (Mathf.Abs(lp.x - fp.x) > Mathf.Abs(lp.y - fp.y))
+                    {   //If the horizontal movement is greater than the vertical movement...
+                        if (lp.x > fp.x)  //If the movement was to the right)
+                        {   //Right swipe
+                            Debug.Log("Right Swipe");
+                        }
+                        // else
+                        // {   //Left swipe
+                        //     Debug.Log("Left Swipe");
+                        // }
+                    }
+                    else
+                    {   //the vertical movement is greater than the horizontal movement
+                        if (lp.y > fp.y)  //If the movement was up
+                        {   //Up swipe
+                            Debug.Log("Up Swipe");
+                        }
+                    }
+                }
+                else
+                {   //It's a tap as the drag distance is less than 20% of the screen height
+                    moveValue = 0;
+                    Debug.Log("Tap");
+                }
+
+            }
+        }
     }
 }
