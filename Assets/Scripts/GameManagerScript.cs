392bb9d629f53910bb1c81a86fe7b803a611d714
Regal Adhikari <65963700+RegalAdhikari@users.noreply.github.com>
2026-04-13 13:49:51 +0545

    mml

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -7,14 +7,5 @@ public class GameManagerScript : MonoBehaviour
     {
         Application.targetFrameRate = 60;
     }
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // unrequired
 }
